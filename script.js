9b1250a201488251579eddafe88b53e0418a51f3
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-04 19:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// write js code if required
+let imgs = document.getElementById('img');
+imgs.addEventListener("mouseenter",()=>{
+	let img2 =  "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
+	imgs.setAttribute("src", img2)
+})
+imgs.addEventListener("mouseleave",()=>{
+	let img2 =  "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
+	imgs.setAttribute("src", img2)
+})
+
