43eb23c93527c9197f52db5d08924120b22b7870
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-04 22:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let imgs = document.getElementById('img');
+let imgs = document.getElementById('flip');
 let img1 = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
 let img2 = " https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
 imgs.addEventListener("mouseenter",()=>{
