1c72c8d60ac2c8539dc47d208d2130798c3858f0
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-04 22:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let imgs = document.getElementById('img');
+let img1 = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
+let img2 = " https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
 imgs.addEventListener("mouseenter",()=>{
-	let img2 =  "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
 	imgs.setAttribute("src", img2)
 })
 imgs.addEventListener("mouseleave",()=>{
-	let img2 =  "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
-	imgs.setAttribute("src", img2)
+	imgs.setAttribute("src", img1)
 })
 
