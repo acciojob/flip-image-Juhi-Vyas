583ab21e302e39c3acb393d80542f9efed9d6823
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-04 23:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let imgs = document.getElementById('flip');
-let img1 = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
-let img2 = " https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
-imgs.addEventListener("mouseenter",()=>{
-	imgs.setAttribute("src", img2)
-})
-imgs.addEventListener("mouseleave",()=>{
-	imgs.setAttribute("src", img1)
-})
+// let imgs = document.getElementById('flip');
+// let img1 = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
+// let img2 = " https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
+// imgs.addEventListener("mouseenter",()=>{
+// 	imgs.setAttribute("src", img2)
+// })
+// imgs.addEventListener("mouseleave",()=>{
+// 	imgs.setAttribute("src", img1)
+// })
 
